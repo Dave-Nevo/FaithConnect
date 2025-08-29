90536d4db74a7eecf67504c397de54991e9945dc
Dave-Nevo <david1ikejimba@gmail.com>
2025-08-29 13:42:05 +0100

    Foot

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header.jsx";
 import Podcast from "../components/Podcast.jsx";
 import Hero from "../components/Hero.jsx";
 import Books from "../components/Books1.jsx";
-import Footer from "../components/footer.jsx";
+import Footer from "../components/Footer.jsx";
 
 const Home = () => {
   return (
